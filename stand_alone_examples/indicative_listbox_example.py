543098402873696a59c7cfa89a871a93f5a95624
AFoeee <44651916+AFoeee@users.noreply.github.com>
2018-11-17 11:46:43 +0100

    Update indicative_listbox_example.py

--- stand_alone_examples/indicative_listbox_example.py
+++ stand_alone_examples/indicative_listbox_example.py
@@ -442,7 +442,7 @@ if __name__ == "__main__":
     # Iterable which holds the labels for the individual list entries.
     ENTRIES = [str(i) for i in range(33)]
     
-    # Color schemes that specifies the appearance off focus and on focus.
+    # Color schemes that specify the appearance off focus and on focus.
     PALETTE = [("default",                   "",                 "black"),
                ("reveal_focus",              "black",            "light cyan",   "standout"),
                ("ilb_barActive_focus",       "dark cyan",        "light gray"),
