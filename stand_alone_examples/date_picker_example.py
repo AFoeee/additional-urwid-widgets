e78219ab38de1f9bdd4c4afe83dda5448364c7b6
AFoeee <44651916+AFoeee@users.noreply.github.com>
2018-11-20 21:45:52 +0100

    Update date_picker_example.py

--- stand_alone_examples/date_picker_example.py
+++ stand_alone_examples/date_picker_example.py
@@ -72,6 +72,8 @@ class SelectableRow(urwid.WidgetWrap):
 
 
 class IndicativeListBox(urwid.WidgetWrap):
+     """Wraps 'urwid.ListBox' to add bars that indicate that only a part of the list is visible."""
+     
     _TYPE_ERR_MSG = "type {} was expected for {}, but found: {}."
     _VALUE_ERR_MSG = "unrecognized value: {}."
 
